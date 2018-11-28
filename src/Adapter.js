b4d7b6eb563ca60a17e6506ccdbda0e32095b315
hsadoqi <hsadoqi@gmail.com>
2018-11-28 13:06:04 -0500

    Done.

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,13 +1,21 @@
+const showsURL = 'http://api.tvmaze.com/shows'
+
 class Adapter {
-  static getShows (){
-    fetch("http://api.tvmaze.com/shows")
+  static getShows(){
+    return fetch(showsURL)
     .then(res => res.json())
   }
 
-  // static getShowEpisodes (showID){
-  //   return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
-  //   .then(res => res.json)
-  // }
+  static getShow(id){
+    return fetch(showsURL + '/' + id)
+    .then(res => res.json())
+  }
+
+  static getShowEpisodes(showID){
+    return fetch(`http://api.tvmaze.com/shows/${showID}/episodes`)
+    .then(res => res.json())
+    // .then(console.log)
+  }
 }
 
 export default Adapter
--- src/Components/SelectedShowContainer.js
+++ src/Components/SelectedShowContainer.js
@@ -1,58 +1,5 @@
 import React, { Component } from 'react';
-import Episode from './Components/Episode';
-
-class SelectedShowContainer extends Component {
-
-  state = {
-    selectedSeason: 1,
-  }
-
-  mapSeasons = () => {
-    if (!!this.props.episodes){
-      let seasons = this.props.episodes.map((e)=> e.season).unique()
-
-      return seasons.map((s) => {
-        return (<option value={s} key={s}>Season {s}</option>)
-      });
-    }
-  }
-
-  mapEpisodes = () => {
-    return this.props.episodes.map((e)=>{
-      if (e.season == this.state.selectedSeason){
-        return (<Episode eachEpisode={e} key={e.id}/>)
-      }
-    })
-  }
-
-  handleSelectionChange = (e) => {
-    this.setState({ selectedSeason: e.target.value })
-  }
-
-
-  render() {
-    const { selectedShow } = this.props
-
-    return (
-      <div style={{position: "static"}}>
-        <h2>{selectedShow.name}</h2>
-        <img src={selectedShow.image.medium} alt=""/>
-        <p dangerouslySetInnerHTML={{__html: selectedShow.summary}}></p>
-        <p>Premiered: {selectedShow.premiered}</p>
-        <p>Status: {selectedShow.status}</p>
-        <p>Average Rating: {selectedShow.rating.average}</p>
-        <select style={{display: 'block'}} onChange={this.handleSelectionChange}>
-          {this.mapSeasons()}
-        </select>
-        {this.mapEpisodes()}
-      </div>
-    );
-  }
-
-}
-
-export SelectedShowContainer;
-
+import Episode from './Episode';
 
 Array.prototype.unique = function() {
   var arr = [];
@@ -63,3 +10,56 @@ Array.prototype.unique = function() {
   }
   return arr;
 }
+
+class SelectedShowContainer extends Component {
+
+  state = {
+    selectedSeason: 1,
+  }
+
+  mapSeasons = (e) => {
+    if (!!this.props.episodes){
+      // console.log(this.props.episodes)
+      let seasons = this.props.episodes.map((e)=> e.season).unique()
+      return seasons.map((s) => {
+        return (<option value={s} key={s}>Season {s}</option>)
+      });
+    }
+  }
+
+  mapEpisodes = () => {
+    return this.props.episodes
+      .filter((e) => e.season === Number(this.state.selectedSeason))
+      .map((e) => <Episode eachEpisode={e} key={e.id}/>)
+      
+  }
+
+  handleSelectionChange = (e) => {
+    this.setState({ selectedSeason: e.target.value })
+  }
+
+
+  render() {
+    const { selectedShow } = this.props
+    // console.log(selectedShow)
+    return (
+      <div style={{position: "static"}}>
+        <h2>{selectedShow.name}</h2>
+        <img src={selectedShow.image.medium} alt=""/>
+        <p dangerouslySetInnerHTML={{__html: selectedShow.summary}}></p>
+        <p>Premiered: {selectedShow.premiered}</p>
+        <p>Status: {selectedShow.status}</p>
+        <p>Average Rating: {selectedShow.rating.average}</p>
+        <select style={{display: 'block'}} value={this.state.selectedSeason} onChange={this.handleSelectionChange}>
+          {this.mapSeasons()}
+        </select>
+        {this.mapEpisodes()}
+      </div>
+    );
+  }
+
+}
+
+export default SelectedShowContainer;
+
+
